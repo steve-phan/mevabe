e0ed3eb6bd138143f244ed824a0f2253ae53080a
Steve Phan <phanhaingoc@gmail.com>
2023-02-06 20:13:42 +0100

    update cypress test

--- cypress/e2e/Home/Home.spec.ts
+++ cypress/e2e/Home/Home.spec.ts
@@ -5,7 +5,10 @@ describe("Home", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("should display the logo", () => {
-    cy.get("a").first().should("exist").should("have.text", "MeVaBe");
+  it("should display the h1 heading", () => {
+    cy.get("h1")
+      .first()
+      .should("exist")
+      .should("have.text", "Thông tin Luật Pháp và cuộc sống tại Đức");
   });
 });
